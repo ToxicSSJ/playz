59809b34ca22fddfd193e45c39f6edd872a6a653
Abraham M. Lora <amlorav@eafit.edu.co>
2020-09-08 23:53:11 -0500

    Revert "Auth added"

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.browserSync('http://localhost:8000/');
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
