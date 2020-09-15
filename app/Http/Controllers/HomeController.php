505dc97eb0b8238b343007109cc07c6c02b355f6
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2020-09-14 21:37:05 -0500

    Fresh install

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Controllers;
 
-class HomeController extends Controller {
+use Illuminate\Http\Request;
 
-  public function index() {
-    return view('home.index');
-  }
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-  public function contact() {
-    return view('home.contact');
-  }
-
-  public function audios() {
-    return view('home.audios');
-  }
-
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
 }
