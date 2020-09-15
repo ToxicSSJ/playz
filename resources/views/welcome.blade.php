60e293bd9357dc0eaee96503bcc85e291a6633ad
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2020-09-15 16:07:48 -0500

    👤 Profile Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,106 +75,81 @@
             </div>
         </div>
 
-        
-    <!-- Services Section -->
-    <div class="services">
-      <h1>See what the hype is about</h1>
-      <div class="services__container">
-        <div class="services__card">
-          <h2>Experience Bliss</h2>
-          <p>AI Powered technology</p>
-          <button>Get Started</button>
-        </div>
-        <div class="services__card">
-          <h2>Are you Ready?</h2>
-          <p>Take the leap</p>
-          <button>Get Started</button>
-        </div>
-      </div>
-    </div>
 
-    <!-- Footer Section -->
-    <div class="footer__container">
-      <div class="footer__links">
-        <div class="footer__link--wrapper">
-          <div class="footer__link--items">
-            <h2>About Us</h2>
-            <a href="/sign__up">How it works</a> <a href="/">Testimonials</a>
-            <a href="/">Careers</a> <a href="/">Investments</a>
-            <a href="/">Terms of Service</a>
-          </div>
-          <div class="footer__link--items">
-            <h2>Contact Us</h2>
-            <a href="/">Contact</a> <a href="/">Support</a>
-            <a href="/">Destinations</a> <a href="/">Sponsorships</a>
-          </div>
+        <!-- Services Section -->
+        <div class="services">
+            <h1>See what the hype is about</h1>
+            <div class="services__container">
+                <div class="services__card">
+                    <h2>Experience Bliss</h2>
+                    <p>AI Powered technology</p>
+                    <button>Get Started</button>
+                </div>
+                <div class="services__card">
+                    <h2>Are you Ready?</h2>
+                    <p>Take the leap</p>
+                    <button>Get Started</button>
+                </div>
+            </div>
         </div>
-        <div class="footer__link--wrapper">
-          <div class="footer__link--items">
-            <h2>Videos</h2>
-            <a href="/">Submit Video</a> <a href="/">Ambassadors</a>
-            <a href="/">Agency</a> <a href="/">Influencer</a>
-          </div>
-          <div class="footer__link--items">
-            <h2>Social Media</h2>
-            <a href="/">Instagram</a> <a href="/">Facebook</a>
-            <a href="/">Youtube</a> <a href="/">Twitter</a>
-          </div>
+
+        <!-- Footer Section -->
+        <div class="footer__container">
+            <div class="footer__links">
+                <div class="footer__link--wrapper">
+                    <div class="footer__link--items">
+                        <h2>About Us</h2>
+                        <a href="/sign__up">How it works</a> <a href="/">Testimonials</a>
+                        <a href="/">Careers</a> <a href="/">Investments</a>
+                        <a href="/">Terms of Service</a>
+                    </div>
+                    <div class="footer__link--items">
+                        <h2>Contact Us</h2>
+                        <a href="/">Contact</a> <a href="/">Support</a>
+                        <a href="/">Destinations</a> <a href="/">Sponsorships</a>
+                    </div>
+                </div>
+                <div class="footer__link--wrapper">
+                    <div class="footer__link--items">
+                        <h2>Videos</h2>
+                        <a href="/">Submit Video</a> <a href="/">Ambassadors</a>
+                        <a href="/">Agency</a> <a href="/">Influencer</a>
+                    </div>
+                    <div class="footer__link--items">
+                        <h2>Social Media</h2>
+                        <a href="/">Instagram</a> <a href="/">Facebook</a>
+                        <a href="/">Youtube</a> <a href="/">Twitter</a>
+                    </div>
+                </div>
+            </div>
+            <section class="social__media">
+                <div class="social__media--wrap">
+                    <div class="footer__logo">
+                        <a href="/" id="footer__logo"><i class="fas fa-compact-disc fa-lg"></i>PLAYZ</a>
+                    </div>
+                    <p class="website__rights">© PLAYZ 2020. All rights reserved</p>
+                    <div class="social__icons">
+                        <a class="social__icon--link" href="/" target="_blank" aria-label="Facebook">
+                            <i class="fab fa-facebook"></i>
+                        </a>
+                        <a class="social__icon--link" href="/" target="_blank" aria-label="Instagram">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a class="social__icon--link" href="//https://www.youtube.com/channel/UCyuYHymUH4Adj2YytTdtD4g" target="_blank" aria-label="Youtube">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                        <a class="social__icon--link" href="/" target="_blank" aria-label="Twitter">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a class="social__icon--link" href="/" target="_blank" aria-label="LinkedIn">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                    </div>
+                </div>
+            </section>
         </div>
-      </div>
-      <section class="social__media">
-        <div class="social__media--wrap">
-          <div class="footer__logo">
-            <a href="/" id="footer__logo"><i class="fas fa-compact-disc fa-lg"></i>PLAYZ</a>
-          </div>
-          <p class="website__rights">© PLAYZ 2020. All rights reserved</p>
-          <div class="social__icons">
-            <a
-              class="social__icon--link"
-              href="/"
-              target="_blank"
-              aria-label="Facebook"
-            >
-              <i class="fab fa-facebook"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="/"
-              target="_blank"
-              aria-label="Instagram"
-            >
-              <i class="fab fa-instagram"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="//https://www.youtube.com/channel/UCyuYHymUH4Adj2YytTdtD4g"
-              target="_blank"
-              aria-label="Youtube"
-            >
-              <i class="fab fa-youtube"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="/"
-              target="_blank"
-              aria-label="Twitter"
-            >
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="/"
-              target="_blank"
-              aria-label="LinkedIn"
-            >
-              <i class="fab fa-linkedin"></i>
-            </a>
-          </div>
-        </div>
-      </section>
     </div>
     <script src="/js/index.js"></script>
-    </div>
 </body>
 
 </html>
