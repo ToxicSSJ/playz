7f0f317de032465258ad7fd965661f1214bd3ff4
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2020-10-04 14:10:04 -0500

    ✅ Some feature Test added

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,12 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
+use App\User;
+
 
 class ExampleTest extends TestCase
 {
@@ -16,6 +21,42 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
+        $response->assertStatus(302);
+    }
+
+    public function testSession()
+    {
+        $response = $this->withSession(['foo' => 'bar'])
+            ->get('/');
+        $response->assertStatus(302);
+    }
+
+    public function testActingAsUser()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->get('/');
         $response->assertStatus(200);
+    }
+
+    // This test is under working
+    public function testAudiosUploadMissing()
+    {
+        Storage::fake('audios');
+
+        $response = $this->json('POST', '/save', [
+            UploadedFile::fake()->image('audio1.mp3'),
+            UploadedFile::fake()->image('audio2.mp3')
+        ]);
+
+        // Assert one or more files were stored...
+        // Storage::disk('audios')->assertExists('audio1.mp3');
+        // Storage::disk('audios')->assertExists(['audio1.mp3', 'audio2.mp3']);
+
+        // Assert one or more files were not stored...
+        Storage::disk('audios')->assertMissing('missing.mp3');
+        Storage::disk('audios')->assertMissing(['missing.mp3', 'non-existing.mp3']);
     }
 }
