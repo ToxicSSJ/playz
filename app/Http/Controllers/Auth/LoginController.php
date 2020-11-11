e8e7214e8baea523b99c6f759449e371bf0e26b2
Abraham M. Lora <abrahamlora3@gmail.com>
2020-11-10 23:57:30 -0500

    uso de User

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,8 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Laravel\Socialite\Facades\Socialite;
 
+use App\User;
+
 class LoginController extends Controller
 {
     /*
